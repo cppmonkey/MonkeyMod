bb002e0ad9d00dc0927837cd418dfe3d90e9b3b6
CppMonkey <parsons151185@gmail.com>
2013-05-09 04:59:42 +0100

    Auto indentation & Netbeans settings

--- src/me/cppmonkey/monkeymod/threads/AnnounceThread.java
+++ src/me/cppmonkey/monkeymod/threads/AnnounceThread.java
@@ -4,57 +4,59 @@ import org.bukkit.ChatColor;
 
 import me.cppmonkey.monkeymod.MonkeyMod;
 
-public class AnnounceThread extends Thread{
-	private Boolean m_halt = false;
-	private MonkeyMod 	m_plugin;
-	private Integer		m_interval = 10000;
-	private String[]	m_msg = {
-			ChatColor.RED+"MonkeyMod is not registered on this server",
-			ChatColor.RED+"Visit " + ChatColor.BLUE + "http://CppMonkey.NET/monkeymod/" + ChatColor.RED + " and register"};
-	
-	public AnnounceThread( MonkeyMod plugin ){
-		m_plugin = plugin;
-	}
-	
-	public AnnounceThread( MonkeyMod plugin, String[] msg ){
-		m_plugin = plugin;
-		m_msg = msg;
-	}
-	
-	public AnnounceThread( MonkeyMod plugin, String[] msg, Integer interval ){
-		m_plugin = plugin;
-		m_msg = msg;
-		m_interval = interval;
+public class AnnounceThread extends Thread {
+
+    private Boolean m_halt = false;
+    private MonkeyMod m_plugin;
+    private Integer m_interval = 10000;
+    private String[] m_msg = {
+        ChatColor.RED + "MonkeyMod is not registered on this server",
+        ChatColor.RED + "Visit " + ChatColor.BLUE + "http://CppMonkey.NET/monkeymod/" + ChatColor.RED + " and register"};
+
+    public AnnounceThread(MonkeyMod plugin) {
+        m_plugin = plugin;
+    }
+
+    public AnnounceThread(MonkeyMod plugin, String[] msg) {
+        m_plugin = plugin;
+        m_msg = msg;
+    }
+
+    public AnnounceThread(MonkeyMod plugin, String[] msg, Integer interval) {
+        m_plugin = plugin;
+        m_msg = msg;
+        m_interval = interval;
     }
     /*
-	* TODO Validate server registration, if found, dont start server
-	*/
-    public Boolean VerifyRegistration(){
-		
-		return true;
-	}
-	
-	public void Halt(){
-		m_halt = true;
-	}
-	
+     * TODO Validate server registration, if found, dont start server
+     */
+
+    public Boolean VerifyRegistration() {
+
+        return true;
+    }
+
+    public void Halt() {
+        m_halt = true;
+    }
+
     @Override
-	public void run(){
-		//While not told to halt, loop
-		while(!m_halt){
-			
-			// Iterate through the message strings, broadcasting
-			for ( int i = 0; i < m_msg.length; i++) {
-				m_plugin.getServer().broadcastMessage(m_msg[i]);
-			}
-			
-			try {
-				// Pause the thread
-				AnnounceThread.sleep( m_interval );
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+    public void run() {
+        //While not told to halt, loop
+        while (!m_halt) {
+
+            // Iterate through the message strings, broadcasting
+            for (int i = 0; i < m_msg.length; i++) {
+                m_plugin.getServer().broadcastMessage(m_msg[i]);
+            }
+
+            try {
+                // Pause the thread
+                AnnounceThread.sleep(m_interval);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 }
