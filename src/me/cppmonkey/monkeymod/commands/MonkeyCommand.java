cc189f5e3869e8e409978e4fa53b7f33a68be09f
CppMonkey <parsons151185@gmail.com>
2013-05-10 14:03:42 +0100

    Implimented live option editing  * setvar and enable/disable implimented - Some settings my need a server/plugin restart

--- src/me/cppmonkey/monkeymod/commands/MonkeyCommand.java
+++ src/me/cppmonkey/monkeymod/commands/MonkeyCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.util.config.Configuration;
 
 public class MonkeyCommand implements CommandExecutor {
 	private final MonkeyMod m_plugin;
@@ -63,11 +64,79 @@ public class MonkeyCommand implements CommandExecutor {
 			
 			
 			if ("enable".equalsIgnoreCase(args[0])||"disable".equalsIgnoreCase(args[0])){
-				// TODO process enable disable commands
-				sender.sendMessage(ChatColor.RED + "Action still to do!");
 				return true;
 			} //END /monkey [enable/disable]
 		}// END args > 0
 		return false;
 	}
+                String[] stringOptions = {
+                    "plugin.update.url",
+                    "logger.url"
+                };
+                
+                if( args.length == 3){
+                    for(int i = 0; i < stringOptions.length;i++){
+                        // Option found
+                        if(stringOptions[i].equalsIgnoreCase(args[1])){
+                            Configuration config = m_plugin.getPluginConfiguration(MonkeyMod.EConfig.PLUGIN);
+                            config.setProperty(
+                                    stringOptions[i],
+                                    args[2]);
+                            config.save();
+                            sender.sendMessage(ChatColor.GREEN+stringOptions[i]+" has been altered");
+                            sender.sendMessage(ChatColor.GREEN+"A restart maybe required to apply changes");
+                            return true;
+                        }
+                    }
+                }
+                
+                String[] intOptions = {
+                    "protection.tower.threshold"
+                };
+                
+                if( args.length == 3){
+                    for(int i = 0; i < intOptions.length;i++){
+                        // Option found
+                        if(intOptions[i].equalsIgnoreCase(args[1])){
+                            Configuration config = m_plugin.getPluginConfiguration(MonkeyMod.EConfig.PLUGIN);
+                            config.setProperty(
+                                    intOptions[i],
+                                    Integer.parseInt(args[2]));
+                            config.save();
+                            sender.sendMessage(ChatColor.GREEN+intOptions[i]+" has been altered");
+                            sender.sendMessage(ChatColor.GREEN+"A restart maybe required to apply changes");
+                            return true;
+                        }
+                    }
+                }
+                // List all all the boolean options
+                String[] boolOptions = {
+                  "plugin.update.auto",  
+                  "protection.grief",
+                  "protection.fire",
+                  "protection.tower.stict",
+                  "protection.tower.enable",
+                  "logger.enable",
+                  "logger.connect",
+                  "logger.disconnect",
+                  "logger.chat",
+                  "override.nag"
+                };
+                
+                // Correct number of arguments?
+                if( args.length == 2){
+                    for(int i = 0; i < boolOptions.length;i++){
+                        // Option found
+                        if(boolOptions[i].equalsIgnoreCase(args[1])){
+                            Configuration config = m_plugin.getPluginConfiguration(MonkeyMod.EConfig.PLUGIN);
+                            config.setProperty(
+                                    boolOptions[i],
+                                    Boolean.parseBoolean(args[0]));
+                            config.save();
+                            sender.sendMessage(ChatColor.GREEN+boolOptions[i]+" has been altered");
+                            sender.sendMessage(ChatColor.GREEN+"A restart maybe required to apply changes");
+                    }
+                }
+                //return false, the command wasnt found
+                return false;
 }
