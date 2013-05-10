6c7be5c533b04f781a8cce1405b83f9a7fe24f7b
CppMonkey <parsons151185@gmail.com>
2013-05-10 14:19:50 +0100

    Mostly whitespace  * difficult to see the trees for the wood

--- src/me/cppmonkey/monkeymod/MonkeyMod.java
+++ src/me/cppmonkey/monkeymod/MonkeyMod.java
@@ -26,217 +26,219 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
-import org.bukkit.util.config.ConfigurationNode;
 
-public class MonkeyMod extends JavaPlugin{
-	
-	//Plugin Details
-	private Integer m_build = 32;
-	
-	private PluginDescriptionFile m_pluginDescFile;
-	
-	private Configuration m_pluginConfig;
-	private Configuration m_pluginPermissions, m_pluginVips;
-	
-	private Stack<MonkeyModThread> m_announceThreads = new Stack<MonkeyModThread>();
-	//Private members containing listeners
-	private final MonkeyModPlayerListener m_PlayerListener = new MonkeyModPlayerListener(this);
-	private final MonkeyModBlockListener m_BlockListener = new MonkeyModBlockListener(this);
-        private final MonkeyModEntityListener m_EntityListener = new MonkeyModEntityListener(this);
-	
-	private static final Logger log = Logger.getLogger("Minecraft");
-	
-	public void onDisable() {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Shutting down MonkeyMod Threads");
-		while (!m_announceThreads.isEmpty()) {
-			MonkeyModThread temp = m_announceThreads.pop();
-			temp.Halt();
-		}
-			
-		
-		log.info( m_pluginDescFile.getFullName() + "(" + m_build + ") is disabled!" );
-	}
+public class MonkeyMod extends JavaPlugin {
 
-	public void onEnable() {
+    //Plugin Details
+    private Integer m_build = 35;
+    private PluginDescriptionFile m_pluginDescFile;
+    private Configuration m_pluginConfig;
+    private Configuration m_pluginPermissions, m_pluginVips;
+    private Stack<MonkeyModThread> m_announceThreads = new Stack<MonkeyModThread>();
+    //Private members containing listeners
+    private final MonkeyModPlayerListener m_PlayerListener = new MonkeyModPlayerListener(this);
+    private final MonkeyModBlockListener m_BlockListener = new MonkeyModBlockListener(this);
+    private final MonkeyModEntityListener m_EntityListener = new MonkeyModEntityListener(this);
+    private static final Logger log = Logger.getLogger("Minecraft");
 
-            setNaggable( true );
-		m_pluginDescFile = this.getDescription();
-		
-		PluginManager pm = getServer().getPluginManager();
-		
-		m_pluginConfig = getConfiguration();
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+        System.out.println("Shutting down MonkeyMod Threads");
+        while (!m_announceThreads.isEmpty()) {
+            MonkeyModThread temp = m_announceThreads.pop();
+            temp.Halt();
+        }
 
-                m_pluginPermissions = new Configuration( new File(getDataFolder(), "permissions.yml") );
-                m_pluginPermissions.load();
 
-		m_pluginVips = new Configuration( new File(getDataFolder(), "vips.yml") );
-                m_pluginVips.load();
-		
-		//Options available to general, just place holders at the moment
-		
-		/*
-		m_pluginConfig.setProperty("server.registered", false);
-		m_pluginConfig.setProperty("plugin.update.auto", false);
-		m_pluginConfig.setProperty("plugin.update.url", "http://cppmonkey.net/minecraft/");
-		m_pluginConfig.setProperty("protection.grief", true);
-		m_pluginConfig.setProperty("protection.tower", true);
-		m_pluginConfig.setProperty("protection.tower.threshold", 40);
-		m_pluginConfig.setProperty("logger.url", "http://cppmonkey.net/minecraft/update.php");
-		m_pluginConfig.setProperty("logger.enable", true);
-		m_pluginConfig.setProperty("logger.connect", true);
-		m_pluginConfig.setProperty("logger.disconnect", true);
-		m_pluginConfig.setProperty("logger.chat", true);
-		
-		m_pluginConfig.setProperty("override.nag", true);
-		*/
-		
-		
-		m_pluginPermissions.save();
-		m_pluginConfig.save();
-		
-		// TODO Server verification before setting up hooks
-		if (!m_pluginConfig.getBoolean("server.registered", false) && !m_pluginConfig.getBoolean("override.nag",false)) {
-			log.info("Creating nag thread");
+        log.info(m_pluginDescFile.getFullName() + "(" + m_build + ") is disabled!");
+    }
 
-                        AnnounceThread announcement = new AnnounceThread(this);
-                        announcement.setPriority( AnnounceThread.MIN_PRIORITY );
-			// FIXME Annoucment thread, need to varify registration
-			announcement.start();
-			m_announceThreads.add(announcement);
-		}
-		
-		log.info( m_pluginDescFile.getFullName() + "(" + m_build +") is enabled!" );
-		
-		// Enable various logger hooks
-		if (m_pluginConfig.getBoolean("logger.enabled", true)) {
-			//Register hooks to process events
-			if(m_pluginConfig.getBoolean("logger.connect", true))
-				pm.registerEvent(Event.Type.PLAYER_JOIN, m_PlayerListener, Priority.Monitor , this);
-			if(m_pluginConfig.getBoolean("logger.disconnect", true))
-				pm.registerEvent(Event.Type.PLAYER_QUIT, m_PlayerListener, Priority.Monitor , this);
-			if(m_pluginConfig.getBoolean("logger.chat", true))
-				pm.registerEvent(Event.Type.PLAYER_CHAT, m_PlayerListener, Priority.Monitor , this);
-		} //END Logging
-		
-		
-		if (m_pluginConfig.getBoolean("protection.grief", true )) {
-			//Stop the burning!!
-			pm.registerEvent(Event.Type.BLOCK_IGNITE, m_BlockListener, Priority.Normal, this);
-		}
+    @Override
+    public void onEnable() {
 
-                pm.registerEvent(Event.Type.BLOCK_PLACE, m_BlockListener, Priority.Normal, this);
-                pm.registerEvent(Event.Type.BLOCK_DAMAGE, m_BlockListener, Priority.Normal, this);
-                pm.registerEvent(Event.Type.BLOCK_BREAK, m_BlockListener, Priority.Normal, this);
+        setNaggable(true);
+        m_pluginDescFile = this.getDescription();
 
-                pm.registerEvent(Event.Type.ENTITY_DEATH, m_EntityListener, Priority.Normal, this);
+        PluginManager pm = getServer().getPluginManager();
 
-                pm.registerEvent(Event.Type.INVENTORY_OPEN , m_PlayerListener, Priority.Normal, this);
+        m_pluginConfig = getConfiguration();
 
-		//TODO Process commands, these a partial commands!!
-		getCommand("monkey").setExecutor(new MonkeyCommand(this));
-		getCommand("item").setExecutor(new ItemCommand(this));
-		getCommand("boxy").setExecutor(new BoxyCommand(this));
-		
-		
-		//Notify CppMonkey.NET of the new server
-		// Notify server about new server
-		
-		String[] parms = {
-	            "action=update",
-	            "package=" + this.getName(),
-	            "version=" + this.getVersion(),
-	            "build=" + this.getBuild(),
-	            "rcon-port=" + (getServer().getPort()+10)
-	        };
+        m_pluginPermissions = new Configuration(new File(getDataFolder(), "permissions.yml"));
+        m_pluginPermissions.load();
+
+        m_pluginVips = new Configuration(new File(getDataFolder(), "vips.yml"));
+        m_pluginVips.load();
+
+        //Options available to general, just place holders at the moment
+
+        /*
+        m_pluginConfig.setProperty("server.registered", false);
+        m_pluginConfig.setProperty("plugin.update.auto", false);
+        m_pluginConfig.setProperty("plugin.update.url", "http://cppmonkey.net/minecraft/");
+        m_pluginConfig.setProperty("protection.grief", true);
+        m_pluginConfig.setProperty("protection.tower", true);
+        m_pluginConfig.setProperty("protection.tower.threshold", 40);
+        m_pluginConfig.setProperty("logger.url", "http://cppmonkey.net/minecraft/update.php");
+        m_pluginConfig.setProperty("logger.enable", true);
+        m_pluginConfig.setProperty("logger.connect", true);
+        m_pluginConfig.setProperty("logger.disconnect", true);
+        m_pluginConfig.setProperty("logger.chat", true);
+        
+        m_pluginConfig.setProperty("override.nag", true);
+         */
+
+
+        m_pluginPermissions.save();
+        m_pluginConfig.save();
+
+        // TODO Server verification before setting up hooks
+        if (!m_pluginConfig.getBoolean("server.registered", false) && !m_pluginConfig.getBoolean("override.nag", false)) {
+            log.info("Creating nag thread");
+
+            AnnounceThread announcement = new AnnounceThread(this);
+            announcement.setPriority(AnnounceThread.MIN_PRIORITY);
+            // FIXME Annoucment thread, need to varify registration
+            announcement.start();
+            m_announceThreads.add(announcement);
+        }
+
+        log.info(m_pluginDescFile.getFullName() + "(" + m_build + ") is enabled!");
+
+        // Enable various logger hooks
+        if (m_pluginConfig.getBoolean("logger.enabled", true)) {
+            //Register hooks to process events
+            if (m_pluginConfig.getBoolean("logger.connect", true)) {
+                pm.registerEvent(Event.Type.PLAYER_JOIN, m_PlayerListener, Priority.Monitor, this);
+            }
+            if (m_pluginConfig.getBoolean("logger.disconnect", true)) {
+                pm.registerEvent(Event.Type.PLAYER_QUIT, m_PlayerListener, Priority.Monitor, this);
+            }
+            if (m_pluginConfig.getBoolean("logger.chat", true)) {
+                pm.registerEvent(Event.Type.PLAYER_CHAT, m_PlayerListener, Priority.Monitor, this);
+            }
+        } //END Logging
+
+
+        if (m_pluginConfig.getBoolean("protection.grief", true)) {
+            //Stop the burning!!
+            pm.registerEvent(Event.Type.BLOCK_IGNITE, m_BlockListener, Priority.Normal, this);
+        }
+
+        pm.registerEvent(Event.Type.BLOCK_PLACE, m_BlockListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_DAMAGE, m_BlockListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_BREAK, m_BlockListener, Priority.Normal, this);
+
+        pm.registerEvent(Event.Type.ENTITY_DEATH, m_EntityListener, Priority.Normal, this);
+
+        pm.registerEvent(Event.Type.INVENTORY_OPEN, m_PlayerListener, Priority.Normal, this);
+
+        //TODO Process commands, these a partial commands!!
+        getCommand("monkey").setExecutor(new MonkeyCommand(this));
+        getCommand("item").setExecutor(new ItemCommand(this));
+        getCommand("boxy").setExecutor(new BoxyCommand(this));
+
+
+        //Notify CppMonkey.NET of the new server
+        // Notify server about new server
+
+        String[] parms = {
+            "action=update",
+            "package=" + this.getName(),
+            "version=" + this.getVersion(),
+            "build=" + this.getBuild(),
+            "rcon-port=" + (getServer().getPort() + 10)
+        };
         HttpRequestThread notification = new HttpRequestThread(
                 "Notification thread: Plugin initialized", new ConsoleCommandSender(getServer()),
                 getLoggerUrl(),
                 parms);
-        
+
         notification.start();
-        
+
         if (m_pluginConfig.getBoolean("plugin.update.auto", false)) {
-	        /*
-	         * Check for updates from server
-	         */
-	        ConsoleCommandSender sender = new ConsoleCommandSender(getServer());
-	        getServer().dispatchCommand(sender, "monkey uptodate");
-        }		
-		//getCommand("debug");
-	}
-	
-	public void selfUpdate( CommandSender sender ){
-		// sender needs to be an OP to carry out this action
-		if (sender.isOp()){
-			try{
-				sender.sendMessage(ChatColor.GREEN + "Trying to update MonkeyMod");
-			}catch(Exception e){
-				log.info( "Unable to message sender");
-			}
-			
-			UpdateThread updateThread = new UpdateThread("Update", sender, this.getName(), "http://cppmonkey.net/minecraft/", this);
-	        updateThread.start();
-		}else{
-			sender.sendMessage(ChatColor.RED + "You dont have permission to do that");
-		}
-	}
-	@Deprecated
-	public Configuration getPluginConfiguration(){
-		return m_pluginConfig;
-	}
-	
-	enum EConfig{
-		PLUGIN,
-		PERMISSIONS,
-		VIP
-	};
-	
-	@Deprecated
-	public Configuration getPluginConfiguration(EConfig config){
-		switch(config){
-		case PLUGIN:
-			return m_pluginConfig;
-		case PERMISSIONS:
-			return m_pluginPermissions;
-		case VIP:
-			return m_pluginVips;
-		}
-		
-		// returns pluginConfig by default
-		return m_pluginConfig;
-	}
+            /*
+             * Check for updates from server
+             */
+            ConsoleCommandSender sender = new ConsoleCommandSender(getServer());
+            getServer().dispatchCommand(sender, "monkey uptodate");
+        }
+        //getCommand("debug");
+    }
 
+    public void selfUpdate(CommandSender sender) {
+        // sender needs to be an OP to carry out this action
+        if (sender.isOp()) {
+            try {
+                sender.sendMessage(ChatColor.GREEN + "Trying to update MonkeyMod");
+            } catch (Exception e) {
+                log.info("Unable to message sender");
+            }
 
-        /*
-         * 
-         */
-        public Boolean getPermition( Player player, String path ){
-            // query permissions file
-            // player.sendMessage(player.getName() + path);
-            return m_pluginPermissions.getBoolean(player.getName() + path, false);
+            UpdateThread updateThread = new UpdateThread("Update", sender, this.getName(), "http://cppmonkey.net/minecraft/", this);
+            updateThread.start();
+        } else {
+            sender.sendMessage(ChatColor.RED + "You dont have permission to do that");
+        }
+    }
+
+    @Deprecated
+    public Configuration getPluginConfiguration() {
+        return m_pluginConfig;
+    }
+
+    public enum EConfig {
+
+        PLUGIN,
+        PERMISSIONS,
+        VIP
+    };
+
+    /*
+     * Retreive configuration file for plugin
+     */
+    public Configuration getPluginConfiguration(EConfig config) {
+        switch (config) {
+            case PLUGIN:
+                return m_pluginConfig;
+            case PERMISSIONS:
+                return m_pluginPermissions;
+            case VIP:
+                return m_pluginVips;
         }
 
-        public Object isKnownUser( Player player ){
-            //FIXME
-            return m_pluginPermissions.getProperty(player.getName().toLowerCase()+".canBuild");
-        }
-	
-	public String getLoggerUrl(){
-		return m_pluginConfig.getString("logger.url", "http://cppmonkey.net/minecraft/update.php");
-	}
-	
-	public String getName()
-	{
-		return m_pluginDescFile.getName();
-	}
-	
-	public String getVersion(){
-		return m_pluginDescFile.getVersion();
-	}
-	
-	public String getBuild(){
-		return m_build.toString();
-	}
+        // returns pluginConfig by default
+        return m_pluginConfig;
+    }
+
+
+    /*
+     * 
+     */
+    public Boolean getPermition(Player player, String path) {
+        // query permissions file
+        // player.sendMessage(player.getName() + path);
+        return m_pluginPermissions.getBoolean(player.getName() + path, false);
+    }
+
+    public Object isKnownUser(Player player) {
+        //FIXME
+        return m_pluginPermissions.getProperty(player.getName().toLowerCase() + ".canBuild");
+    }
+
+    public String getLoggerUrl() {
+        return m_pluginConfig.getString("logger.url", "http://cppmonkey.net/minecraft/update.php");
+    }
+
+    public String getName() {
+        return m_pluginDescFile.getName();
+    }
+
+    public String getVersion() {
+        return m_pluginDescFile.getVersion();
+    }
+
+    public String getBuild() {
+        return m_build.toString();
+    }
 }
--- src/me/cppmonkey/monkeymod/commands/MonkeyCommand.java
+++ src/me/cppmonkey/monkeymod/commands/MonkeyCommand.java
@@ -10,65 +10,54 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.util.config.Configuration;
 
 public class MonkeyCommand implements CommandExecutor {
-	private final MonkeyMod m_plugin;
-	
-	
-	public MonkeyCommand(MonkeyMod instance){
-		m_plugin = instance;
-	}
-	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-		//TODO process commands
-		// Priority to /monkey update!
-		if (args.length > 0) {
-			if ("update".equalsIgnoreCase( args[0])) {
-				//Let the user know what's going on		
-				m_plugin.selfUpdate(sender);            
-				return true;
-			} //END /monkey update
-			
-			// force update check
-			if ("uptodate".equalsIgnoreCase(args[0])){
-				// Setting up parms for http update query
-				String[] parms = {
-						"name=" + m_plugin.getName(),
-						"version=" + m_plugin.getVersion(),
-						"build=" + m_plugin.getBuild()
-				};
-				
-				// Create http request thread 
-				HttpRequestThread updateQuery = new HttpRequestThread("uptodate", sender, "http://cppmonkey.net/monkeymod/ajax.php", parms, m_plugin );
-				// Start the thread
-				updateQuery.start();
-				
-				return true;
-			}  //END /monkey uptodate
-			
-			// process /monkey version
-			if ("version".equalsIgnoreCase(args[0])){
-				sender.sendMessage(ChatColor.RED + m_plugin.getName() + " " + m_plugin.getVersion() + "(" + m_plugin.getBuild() + ")");
-				return true;
-			} //END /monkey version
-			
-			if ("status".equalsIgnoreCase(args[0])){
-				// TODO process status commands
-				sender.sendMessage(ChatColor.RED + "Action still to do!");
-				return true;
-			} //END /monkey status
-			
-			if ("setvar".equalsIgnoreCase(args[0])){
-				// TODO process setvar commands
-				sender.sendMessage(ChatColor.RED + "Action still to do!");
-				return true;
-			} //END /monkey setvar
-			
-			
-			if ("enable".equalsIgnoreCase(args[0])||"disable".equalsIgnoreCase(args[0])){
-				return true;
-			} //END /monkey [enable/disable]
-		}// END args > 0
-		return false;
-	}
+
+    private final MonkeyMod m_plugin;
+
+    public MonkeyCommand(MonkeyMod instance) {
+        m_plugin = instance;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        //TODO process commands
+        // Priority to /monkey update!
+        if (args.length > 0) {
+            if ("update".equalsIgnoreCase(args[0])) {
+                //Let the user know what's going on		
+                m_plugin.selfUpdate(sender);
+                return true;
+            } //END /monkey update
+
+            // force update check
+            if ("uptodate".equalsIgnoreCase(args[0])) {
+                // Setting up parms for http update query
+                String[] parms = {
+                    "name=" + m_plugin.getName(),
+                    "version=" + m_plugin.getVersion(),
+                    "build=" + m_plugin.getBuild()
+                };
+
+                // Create http request thread 
+                HttpRequestThread updateQuery = new HttpRequestThread("uptodate", sender, "http://cppmonkey.net/monkeymod/ajax.php", parms, m_plugin);
+                // Start the thread
+                updateQuery.start();
+
+                return true;
+            }  //END /monkey uptodate
+
+            // process /monkey version
+            if ("version".equalsIgnoreCase(args[0])) {
+                sender.sendMessage(ChatColor.RED + m_plugin.getName() + " " + m_plugin.getVersion() + "(" + m_plugin.getBuild() + ")");
+                return true;
+            } //END /monkey version
+
+            if ("status".equalsIgnoreCase(args[0])) {
+                // TODO process status commands
+                sender.sendMessage(ChatColor.RED + "Action still to do!");
+                return true;
+            } //END /monkey status
+
+            if ("setvar".equalsIgnoreCase(args[0])) {
+                // TODO process setvar commands
                 String[] stringOptions = {
                     "plugin.update.url",
                     "logger.url"
@@ -109,6 +98,12 @@ public class MonkeyCommand implements CommandExecutor {
                         }
                     }
                 }
+                sender.sendMessage(ChatColor.RED + "Action still to do!");
+                return true;
+            } //END /monkey setvar
+
+
+            if ("enable".equalsIgnoreCase(args[0]) || "disable".equalsIgnoreCase(args[0])) {
                 // List all all the boolean options
                 String[] boolOptions = {
                   "plugin.update.auto",  
@@ -135,8 +130,14 @@ public class MonkeyCommand implements CommandExecutor {
                             config.save();
                             sender.sendMessage(ChatColor.GREEN+boolOptions[i]+" has been altered");
                             sender.sendMessage(ChatColor.GREEN+"A restart maybe required to apply changes");
+                            return true;
+                        }
                     }
                 }
                 //return false, the command wasnt found
                 return false;
+            } //END /monkey [enable/disable]
+        }// END args > 0
+        return false;
+    }
 }
